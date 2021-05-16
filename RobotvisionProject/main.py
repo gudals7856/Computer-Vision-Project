9d330c772adf5ee105518efb16b7d86b5962da17
gudals7856 <gudals7856@gmail.com>
2021-05-16 19:28:06 +0900

    optical flow 수정

--- RobotvisionProject/main.py
+++ RobotvisionProject/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 img1_origin = cv2.imread('frame1.jpg')
 img2_origin = cv2.imread('frame2.jpg')
@@ -26,7 +26,7 @@ def lukasKanade(x, y): # 1280, 720
             tmpy = y + m
             tmpx = x + n
 
-            ay = int(gray1[tmpy + 1, tmpx]) - int(gray1[tmpy, tmpx])
+            ay = int(gray1[tmpy+1, tmpx]) - int(gray1[tmpy, tmpx])
             A[num][0] = ay
             ax = int(gray1[tmpy, tmpx+1]) - int(gray1[tmpy, tmpx])
             A[num][1] = ax
@@ -41,26 +41,44 @@ def lukasKanade(x, y): # 1280, 720
     # 역행렬이 존재하지 않는 경우 예외. 자기 자신을 반환
     if np.linalg.det(tmp) == 0:
         return x, y
-    
+
     # 역행렬 계산 후 vt계산
     inverse_arr = np.linalg.inv(tmp)
     vt = np.dot(np.dot(inverse_arr, A.T), b)
-    vt_n = vt / np.linalg.norm(vt) * 10
+
+    if np.linalg.norm(vt) == 0:
+        return x, y
+
+    vt_n = vt / np.linalg.norm(vt) * 5
 
     np.nan_to_num(vt_n, copy=False)
 
-    print(vt_n)
-
     return vt_n
 
+
+# x1 = np.zeros((720, 1280), dtype=int)
+# y1 = np.zeros((720, 1280), dtype=int)
+# u = np.zeros((720, 1280), dtype=float)
+# v = np.zeros((720, 1280), dtype=float)
+
 # pt1과 pt2를 화면에 표시
-for x in range(10, width, 10):  # 1280
-    for y in range(10, height, 10):  # 720
+for x in range(1, width-2, 10):  # 1280
+    for y in range(1, height-2, 10):  # 720
 
         vt = lukasKanade(x, y)
-        cv2.arrowedLine(img1, (x, y), (int(x + vt[0]), int(y + vt[1])), (255, 0, 0), 1)
+        cv2.arrowedLine(img1, (x, y), (x + int(vt[0]), y + int(vt[1])), (255, 0, 0), 1)
+
+        # x1[y][x] = x
+        # y1[y][x] = y
+        # u[y][x] = vt[0]
+        # v[y][x] = vt[1]
     else:
         continue
+
+# plt.quiver(x1, y1, v, u)
+# plt.ylim(720, 0)
+# plt.xlim(0, 1280)
+# plt.show()
 
 cv2.imshow('result', img1)
 cv2.waitKey()
