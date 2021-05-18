68d41960baa679520834c2a5ba55abbdc0d96869
gudals7856 <gudals7856@gmail.com>
2021-05-18 23:03:27 +0900

    mean shift

--- MeanShift/main.py
+++ MeanShift/main.py
@@ -2,5 +2,19 @@ import cv2
 import numpy as np
 
 img1_origin = cv2.imread('frame1.jpg')
-B, G, R = cv2.split('frame1.jpg')
+img1 = cv2.resize(img1_origin, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
 
+height, width, _ = img1_origin.shape #720 1280
+B, G, R = cv2.split(img1_origin)
+rgb_data = np.zeros((256, 256, 256), np.uint8)
+location_data = np.zeros((720, 1280), np.uint8)
+
+for x in (0, 1280):
+    for y in (0, 720):
+        r = R[y, x]
+        g = G[y, x]
+        b = B[y, x]
+
+cv2.imshow('result', img1_origin)
+cv2.waitKey()
+cv2.destroyAllWindows()
