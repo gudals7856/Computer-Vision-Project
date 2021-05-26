f949225c3b09b6af1c20b2263cc2c635126290c0
gudals7856 <gudals7856@gmail.com>
2021-05-26 14:03:05 +0900

    optical flow 수정

--- RobotvisionProject/main.py
+++ RobotvisionProject/main.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread('frame1.jpg')
-img2 = cv2.imread('frame2.jpg')
+img1_origin = cv2.imread('f1.jpg')
+img2_origin = cv2.imread('f2.jpg')
 
-# img1 = cv2.resize(img1_origin, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
-# img2 = cv2.resize(img2_origin, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
+img1 = cv2.resize(img1_origin, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
+img2 = cv2.resize(img2_origin, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
 
 height, width, _ = img1.shape  # 720 1280 (y, x)
 
@@ -13,9 +13,8 @@ height, width, _ = img1.shape  # 720 1280 (y, x)
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-dirX = [1, -1, 0]
-dirY = [1, -1, 0]
-
+dirX = [1, 0, -1]
+dirY = [1, 0, -1]
 
 # lukas-kanade 알고리즘 적용
 def lukasKanade(x, y):  # 1280, 720
@@ -24,8 +23,8 @@ def lukasKanade(x, y):  # 1280, 720
 
     # 자신과 주위8개 픽셀에 대해 모두 수행
     num = 0
-    for m in dirX:
-        for n in dirY:
+    for m in dirX:          # dirX = [1, 0, -1]
+        for n in dirY:      # dirY = [1, 0, -1]
             tmpy = y + n
             tmpx = x + m
 
@@ -43,7 +42,6 @@ def lukasKanade(x, y):  # 1280, 720
 
     # 역행렬이 존재하지 않는 경우 예외.
     if np.linalg.det(tmp) == 0:
-        # cv2.arrowedLine(img1, (x, y), (int(x), int(y)), (255, 0, 0), 1)
         return
 
     # 역행렬 계산 후 vt를 계산한다.
@@ -52,18 +50,15 @@ def lukasKanade(x, y):  # 1280, 720
 
     # 0으로 나누어지는 경우는 계산하지 않는다.
     if np.linalg.norm(vt) == 0:
-        # cv2.arrowedLine(img1, (x, y), (int(x), int(y)), (255, 0, 0), 1)
         return
 
     vt_n = vt / np.linalg.norm(vt) * 7
 
     np.nan_to_num(vt_n, copy=False)
 
-    cv2.arrowedLine(img1, (x, y), (int(x + vt_n[1]), int(y + vt_n[0])), (0, 250, 0), 1)
+    cv2.arrowedLine(img1, (x, y), (int(x + vt_n[1]), int(y + vt_n[0])), (0, 0, 255), 1)
     return
 
-
-# pt1과 pt2를 화면에 표시
 for x in range(0, width, 10):  # 1280
     for y in range(0, height, 10):  # 720
         lukasKanade(x, y)
